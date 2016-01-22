a27f25f2c34c4e4176ed752842743d6b041e9ece
makweb <makweb@yandex.ru>
2016-01-22 12:39:47 +0400

    webinar 1 completed

--- app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
@@ -1,16 +1,19 @@
 package com.softdesign.school.ui.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.softdesign.school.R;
+import com.softdesign.school.utils.Lg;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,27 +23,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String COUNT_KEY = "COUNT";
     private int mCount;
     private Toolbar mToolbar;
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("SCHOOL", "onCreate");
+        Lg.e(this.getClass().getSimpleName(), "onCreate");
         mCheckBox = (CheckBox) findViewById(R.id.checkBox);
         mEditText = (EditText) findViewById(R.id.text_field_2);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button btnBlue = (Button) findViewById(R.id.button_blue);
+        Button btnGreen = (Button) findViewById(R.id.button_green);
+        Button btnRed = (Button) findViewById(R.id.button_red);
+
         mCheckBox.setOnClickListener(this);
+        btnBlue.setOnClickListener(this);
+        btnRed.setOnClickListener(this);
+        btnGreen.setOnClickListener(this);
+
         setupToolbar();
-        Toast mToast;
+        setTitle(this.getClass().getSimpleName());
 
         if (savedInstanceState != null) {
-            int visibleState = savedInstanceState.getBoolean(VISABLE_KEY ) ? View.VISIBLE : View.INVISIBLE;
+            int visibleState = savedInstanceState.getBoolean(VISABLE_KEY) ? View.VISIBLE : View.INVISIBLE;
             mCount = savedInstanceState.getInt(COUNT_KEY);
             mCount++;
             mEditText.setVisibility(visibleState);
             mToast = Toast.makeText(this, "активити создано " + mCount + " раз", Toast.LENGTH_SHORT);
-        } else{
+        } else {
             mToast = Toast.makeText(this, "активити создано впервые", Toast.LENGTH_SHORT);
         }
         mToast.show();
@@ -49,45 +61,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("SCHOOL", "onStart()");
+        Lg.e(this.getClass().getSimpleName(), "onStart()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e("SCHOOL", "onResume()");
+        Lg.e("TAG", "onResume()");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e("SCHOOL", "onPause()");
+        Lg.e(this.getClass().getSimpleName(), "onPause()");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("SCHOOL", "onStop()");
+        Lg.e(this.getClass().getSimpleName(), "onStop()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e("SCHOOL", "onDestroy()");
+        Lg.e(this.getClass().getSimpleName(), "onDestroy()");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.e("SCHOOL", "onRestart()");
+        Lg.e(this.getClass().getSimpleName(), "onRestart()");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.e("SCHOOL", "onRestoreInstanceState()");
-        Log.e("SCHOOL", String.valueOf(savedInstanceState.getBoolean(VISABLE_KEY)));
-        int visibleState = savedInstanceState.getBoolean(VISABLE_KEY ) ? View.VISIBLE : View.INVISIBLE;
+        Lg.e(this.getClass().getSimpleName(), "onRestoreInstanceState()");
+        Lg.e(this.getClass().getSimpleName(), String.valueOf(savedInstanceState.getBoolean(VISABLE_KEY)));
+        int visibleState = savedInstanceState.getBoolean(VISABLE_KEY) ? View.VISIBLE : View.INVISIBLE;
 
         mEditText.setVisibility(visibleState);
     }
@@ -95,22 +107,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.e("SCHOOL", "onSaveInstanceState()");
+        Lg.e(this.getClass().getSimpleName(), "onSaveInstanceState()");
         outState.putBoolean(VISABLE_KEY, mEditText.getVisibility() == View.VISIBLE);
         outState.putInt(COUNT_KEY, mCount);
-        Log.e("SCHOOL", String.valueOf(mEditText.getVisibility() == View.VISIBLE));
+        Lg.e(this.getClass().getSimpleName(), String.valueOf(mEditText.getVisibility() == View.VISIBLE));
     }
 
     @Override
     public void onClick(View view) {
         int id = view.getId();
-        if (view.getId() == R.id.checkBox) {
-            if (mCheckBox.isChecked()) {
-                mEditText.setVisibility(View.INVISIBLE);
-            } else {
-                mEditText.setVisibility(View.VISIBLE);
-            }
+        switch (id) {
+            case R.id.checkBox:
+                if (mCheckBox.isChecked()) {
+                    mEditText.setVisibility(View.INVISIBLE);
+                } else {
+                    mEditText.setVisibility(View.VISIBLE);
+                }
+                break;
+
+            case R.id.button_green:
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.green));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.green_dark));
+                }
+                break;
+
+            case R.id.button_blue:
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.primary));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.primary_dark));
+                }
+                break;
+
+            case R.id.button_red:
+                mToolbar.setBackgroundColor(getResources().getColor(R.color.red));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    getWindow().setStatusBarColor(getResources().getColor(R.color.red_dark));
+                }
+                break;
+
         }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mToast = Toast.makeText(this, "Здесь будет меню", Toast.LENGTH_SHORT);
+            mToast.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void setupToolbar() {
@@ -120,6 +166,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
     }
 }
