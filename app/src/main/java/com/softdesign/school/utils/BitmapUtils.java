6da8aa8d17495fd43660ba52bdb28fd85db8f228
A.Tretyak <alextret91@yandex.ru>
2016-01-27 17:36:51 +0400

    Сверстан NavigationDrawer

---
+++ app/src/main/java/com/softdesign/school/utils/BitmapUtils.java
@@ -0,0 +1,56 @@
+package com.softdesign.school.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+
+/**
+ * Created by ONLY-ONE on 27.01.2016.
+ */
+public class BitmapUtils {
+    public static Bitmap getCircleMaskedBitmapUsingShader(Bitmap source, int radius)
+    {
+        if (source == null)
+        {
+            return null;
+        }
+
+        int diam = radius << 1;
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        Bitmap scaledBitmap = scaleTo(source, diam);
+        final Shader shader = new BitmapShader(scaledBitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        paint.setShader(shader);
+
+        Bitmap targetBitmap = Bitmap.createBitmap(diam, diam, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(targetBitmap);
+
+        canvas.drawCircle(radius, radius, radius, paint);
+
+        return targetBitmap;
+    }
+
+    private static Bitmap scaleTo(Bitmap source, int size) {
+        int destWidth = source.getWidth();
+
+        int destHeight = source.getHeight();
+
+        destHeight = destHeight * size / destWidth;
+        destWidth = size;
+
+        if (destHeight < size)
+        {
+            destWidth = destWidth * size / destHeight;
+            destHeight = size;
+        }
+
+        Bitmap destBitmap = Bitmap.createBitmap(destWidth, destHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(destBitmap);
+        canvas.drawBitmap(source, new Rect(0, 0, source.getWidth(), source.getHeight()), new Rect(0, 0, destWidth, destHeight), new Paint(Paint.ANTI_ALIAS_FLAG));
+        return destBitmap;
+    }
+}
